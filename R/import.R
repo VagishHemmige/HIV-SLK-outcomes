c11fa9e769df2b5b398422f467587e4cbbb610ef
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-06 22:11:47 -0500

    Commit website

---
+++ R/import.R
@@ -0,0 +1,7 @@
+
+
+#Import raw kidney and liver files
+tx_li <- load_srtr_file("tx_li", factor_labels = TRUE, var_labels = TRUE)
+tx_ki <- load_srtr_file("tx_ki", factor_labels = TRUE, var_labels = TRUE)
+
+
