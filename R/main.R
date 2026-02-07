5e7a029f39149ae2a043ead38c4b96612e8888dc
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-07 14:04:29 -0500

    Create main.R script

---
+++ R/main.R
@@ -0,0 +1,13 @@
+#Main R script that runs other R scripts in order
+
+source("R/setup.R")
+source("R/functions.R")
+
+
+source("R/import.R")
+source("R/excluding_prior_transplants.R")
+source("R/combining_liver_kidney_variables.R")
+
+
+source("R/tables.R")
+source("R/figures.R")
