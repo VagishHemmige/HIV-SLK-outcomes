2ecd302c64a470f6fc43b943cf222976aef73092
Vagish Hemmige <vahemmig@montefiore.org>
2026-02-08 19:08:19 -0500

    Update setup file

--- R/main.R
+++ R/main.R
@@ -1,9 +1,12 @@
 #Main R script that runs other R scripts in order
 
+#Initialization
 source("R/setup.R")
+
+#Files that define convenience functions for use in the analysis
 source("R/functions.R")
 
-
+#Primary data prepration
 source("R/import.R")
 source("R/excluding_prior_transplants.R")
 source("R/combining_liver_kidney_variables.R")
@@ -12,6 +15,7 @@ source("R/create_liver_only_dataset.R")
 source("R/add_donor_data.R")
 source("R/add_candidate_data.R")
 
+#Modeling
 
 
 source("R/tables.R")
